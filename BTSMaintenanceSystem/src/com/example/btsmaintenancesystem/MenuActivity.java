0a56889640e90fe509dd87a7a75e378b6ac22f98
Slyboots92 <michal.blach92@gmail.com>
2013-11-25 01:54:02 +0100

    First activity

--- BTSMaintenanceSystem/src/com/example/btsmaintenancesystem/MenuActivity.java
+++ BTSMaintenanceSystem/src/com/example/btsmaintenancesystem/MenuActivity.java
@@ -1,42 +1,27 @@
 package com.example.btsmaintenancesystem;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.View;
 
-
-public class MainActivity extends Activity {
+public class MenuActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.intro_activity);
-		
+		setContentView(R.layout.menu_activity);
 		
 	
 		
 		
 	}
-	
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		
-		   
-		
 		return true;
-	}
-	public void  startMenu(View view)
-	{
-		Intent intent = new Intent(this,MenuActivity.class);
-	
-		startActivity(intent);
-		finish();
-		
 	}
 
 }
