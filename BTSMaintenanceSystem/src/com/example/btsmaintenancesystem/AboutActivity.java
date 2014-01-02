2f160d8e6fb2f4512bb98ea5288814691fa4397c
Slyboots92 <michal.blach92@gmail.com>
2014-01-02 06:50:14 +0100

    About Activity done

--- BTSMaintenanceSystem/src/com/example/btsmaintenancesystem/AboutActivity.java
+++ BTSMaintenanceSystem/src/com/example/btsmaintenancesystem/AboutActivity.java
@@ -1,45 +1,35 @@
 package com.example.btsmaintenancesystem;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 
-public class MenuActivity extends Activity {
+
+public class AboutActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.menu_activity);
+		setContentView(R.layout.about_activity);
+		
 		
 	
 		
 		
 	}
-
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		
+		   
+		
 		return true;
 	}
-	public void  searchMenu(View view)
-	{
-		Intent intent = new Intent(this,SearchActivity.class);
 	
-		startActivity(intent);
-		
-		
-	}
-	public void  aboutMenu(View view)
-	{
-		Intent intent = new Intent(this,AboutActivity.class);
-	
-		startActivity(intent);
-		
-		
-	}
 
 }
